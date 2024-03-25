daafcfa70257f8f0291d80195b9bc2bef3fcf183
KyronGreen6 <kyrongreen@pursuit.org>
2024-03-25 12:43:27 -0400

    Completed Nested Contexts exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import ThemeSwitcher from './ThemeSwitcher';
+import LanguageSelector from './LanguageSelector';
+import { ThemeProvider } from './ThemeContext';
+import { LanguageProvider } from './LanguageContext';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LanguageProvider>
+      <ThemeProvider>
+        <div>
+          <ThemeSwitcher />
+          <LanguageSelector />
+        </div>
+      </ThemeProvider>
+    </LanguageProvider>
   );
-}
+};
 
 export default App;
